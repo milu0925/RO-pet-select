e30506ff165295a6dbdb28b41d2423aaa770b46f
milu0925u <elk820925@gmail.com>
2024-11-19 14:57:14 +0800

    WWW

--- component/sidebar/monster-list.js
+++ component/sidebar/monster-list.js
@@ -1,36 +1,44 @@
 import { useCheck } from "@/hook/check-context";
 import React from "react";
 import Image from "next/image";
-export default function MonsterList() {
-  const { prop } = useCheck();
+export default function SidebarMonsterList() {
+  const { prop, monster } = useCheck();
+  console.log(monster);
+  console.log(prop);
 
   return (
     <>
-      <div className="one-monster-data">
-        <div className="img">
-          <Image
-            src={`/images/${prop.key}.png`}
-            alt="monster"
-            width="150"
-            height="150"
-          />
-        </div>
-        <div className="label">
-          <div className="attr">
-            {prop.value?.[0] || ""}
-            {/* {!prop.value?.[0]?.includes("屬性") ? "屬性" : ""} */}
+      {monster ? (
+        <>
+          <div className="one-monster-data">
+            <div className="img">
+              <Image
+                src={`/images/${prop.key}.png`}
+                alt="monster"
+                width="150"
+                height="150"
+              />
+            </div>
+            <div className="label">
+              <div className="attr">
+                {prop.value?.[0] || ""}
+                {!prop.value?.[0]?.includes("屬性") ? "屬性" : ""}
+              </div>
+              <div className="eth">{prop.value[2]}</div>
+              <div className="shape">{prop.value[1]}</div>
+            </div>
+            <div className="label1">
+              <div className="level">Lv.{prop.value[3]}</div>
+              <div className="name">{prop.key}</div>
+            </div>
           </div>
-          <div className="eth">{prop.value[2]}</div>
-          <div className="shape">{prop.value[1]}</div>
-        </div>
-        <div className="label1">
-          <div className="level">Lv.{prop.value[3]}</div>
-          <div className="name">{prop.key}</div>
-        </div>
-      </div>
-      <div className="mater">
-        材料：{prop.value[4] ? prop.value[4].map((v) => `${v} `) : ""}
-      </div>
+          <div className="mater">
+            材料：{prop.value[4] ? prop.value[4].map((v) => `${v} `) : ""}
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 }
