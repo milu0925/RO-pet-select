00dde920e95515ebb55dd909a268a67a0ae5b3d3
milu0925u <elk820925@gmail.com>
2024-11-20 10:14:55 +0800

    nav

--- component/content/card.js
+++ component/content/card.js
@@ -9,35 +9,39 @@ export default function Card() {
   return (
     <div className="flex">
       {currentM &&
-        Object.entries(currentM).map(([key, values], index) => (
-          <label
-            key={key}
-            htmlFor={key}
-            className={`black-block monster ${
-              aid.includes(key) ? "active" : ""
-            }`}
-          >
-            <div className="image-container">
-              <img alt="image" src={`/images/${key}.png`} />
-            </div>
+        Object.entries(currentM).map(([key, values], index) => {
+          let filteredValues = values[0].slice(0, 2); //只在顯示中去除
 
-            <div className="prop">
-              <span>{values[0]}</span>
-              <span>{values[1]}</span>
-              <span>{values[2]}</span>
-            </div>
-            <div className="black-block title">{key}</div>
-            <input
-              id={key}
-              name="fight"
-              type="checkbox"
-              checked={aid.includes(key)}
-              onChange={(event) => {
-                handlepush(event, key, values);
-              }}
-            />
-          </label>
-        ))}
+          return (
+            <label
+              key={key}
+              htmlFor={key}
+              className={`black-block monster ${
+                aid.includes(key) ? "active" : ""
+              }`}
+            >
+              <div className="image-container">
+                <img alt="image" src={`/images/${key}.png`} />
+              </div>
+
+              <div className="prop">
+                <span>{filteredValues}</span>
+                <span>{values[1]}</span>
+                <span>{values[2]}</span>
+              </div>
+              <div className="black-block title">{key}</div>
+              <input
+                id={key}
+                name="fight"
+                type="checkbox"
+                checked={aid.includes(key)}
+                onChange={(event) => {
+                  handlepush(event, key, values);
+                }}
+              />
+            </label>
+          );
+        })}
     </div>
   );
 }
