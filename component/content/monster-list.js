2c29bf28c88e9484a0bd17a3ebabb984e69b1876
milu0925u <elk820925@gmail.com>
2024-12-03 11:32:21 +0800

    F

--- component/content/monster-list.js
+++ component/content/monster-list.js
@@ -65,6 +65,25 @@ export default function ContentMonsterList() {
     centerMode: false,
     focusOnSelect: false,
     touchThreshold: 5, // 調整滑動閾值
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+    ],
   };
 
   // 手機板
