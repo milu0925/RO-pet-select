6340861166ffc27bcc7223e7e6400df838c3377e
milu0925u <elk820925@gmail.com>
2024-12-03 10:08:25 +0800

    new

--- component/content/monster-list.js
+++ component/content/monster-list.js
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { useSelect } from "@/hook/select-context";
 import MCard from "./m-card";
-import { strengthen, enchant, refine, repair } from "../json/ro";
+import {
+  strengthen,
+  enchant,
+  refine,
+  repair,
+  protect,
+  other,
+} from "../json/ro";
 
 import Slider from "react-slick";
 import MonsterMobile from "../mobile/monster-mobile";
@@ -19,14 +26,21 @@ export default function ContentMonsterList() {
 
   // 計算滑動位置
   const calculatePosition = () => {
-    const buttonNames = ["strengthen", "enchant", "refine", "repair"];
+    const buttonNames = [
+      "strengthen",
+      "enchant",
+      "refine",
+      "repair",
+      "protect",
+      "other",
+    ];
     const index = buttonNames.indexOf(active);
     return index * 100;
   };
 
   // 獲取當前的數組數據
   const getActiveArray = () => {
-    const arrays = { strengthen, enchant, refine, repair };
+    const arrays = { strengthen, enchant, refine, repair, protect, other };
     return arrays[active] || [];
   };
 
@@ -65,7 +79,14 @@ export default function ContentMonsterList() {
       <div className="top">
         {!isMobile ? (
           <div className="black-block scroll-search">
-            {["strengthen", "enchant", "refine", "repair"].map((name) => (
+            {[
+              "strengthen",
+              "enchant",
+              "refine",
+              "repair",
+              "protect",
+              "other",
+            ].map((name) => (
               <button
                 key={name}
                 name={name}
@@ -78,7 +99,11 @@ export default function ContentMonsterList() {
                   ? "附魔"
                   : name === "refine"
                   ? "精煉"
-                  : "修復"}
+                  : name === "repair"
+                  ? "修復"
+                  : name === "protect"
+                  ? "保護"
+                  : "其他"}
               </button>
             ))}
             <div
