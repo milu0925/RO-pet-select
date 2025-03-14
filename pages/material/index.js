b7b9c7cfda38d0e7bfb9bf01030d242c0b6e26e2
milu0925u <milu17y@gmail.com>
2025-03-14 15:07:33 +0800

    給key

--- pages/material/index.js
+++ pages/material/index.js
@@ -104,8 +104,8 @@ export default function Material() {
             <div className="word-arrangement px-2 py-4">
               <div className="fw-bold">{click.type === "special" ? "稀有" : "一般"}寵物</div>
               <div>
-                <div><small>原有素材：</small>{click.item.map((v)=>(<span className="px-1">{v}</span>))}</div>
-                <div><small>可換素材：</small>{click.attach.map((v)=>(v))}</div>
+                <div><small>原有素材：</small>{click.item.map((v,i)=>(<span key={i} className="px-1">{v}</span>))}</div>
+                <div><small>可換素材：</small>{click.attach.map((v,i)=>(<span key={i} className="px-1">{v}</span>))}</div>
               </div>
             </div>
           </div>
